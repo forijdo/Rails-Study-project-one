4d8ba3044b68164374287a4431a049352592de18
forijdo <forijdo@gmail.com>
2019-01-17 15:31:26 +0300

    Remove console form PagesController action index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
 
     def index
-        #logger.info request.env
-
-        console
-
         @http_request_params = request.env.select{ |k,v| k.include?('HTTP') }
         @remote_addr         = request.env['REMOTE_ADDR']
     end
