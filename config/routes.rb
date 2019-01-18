699323f4c90be594ce5e687e62f98e0bded1216c
forijdo <forijdo@gmail.com>
2019-01-18 11:03:43 +0300

    - Include name attribute to sign_up_params and account_update_params   methods inside the RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root         to: 'pages#index'
   get 'index', to: 'pages#index'
 
-  devise_for   :users
+  devise_for   :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
