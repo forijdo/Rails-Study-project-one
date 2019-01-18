b1e8bad7d1cd08c8cfe1d8aa91b87fdfbb9bb3a1
forijdo <forijdo@gmail.com>
2019-01-18 10:59:01 +0300

    Добавление дополнительного поля `name` при мигранции гемом `device`

--- db/migrate/20190118061023_devise_create_users.rb
+++ db/migrate/20190118061023_devise_create_users.rb
@@ -32,8 +32,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
+
+      # Дополнительные поля
+      t.string :name, null: false, default: ""
+
     end
 
     add_index :users, :email,                unique: true
